3a9609d2b508399a594f899865f69a7e25aac0ec
Vincent Oke <nisotus@gmail.com>
2022-09-08 19:11:58 +0200

    Second file

---
+++ 1-What_is_Python.py
@@ -0,0 +1,9 @@
+# Why Python?
+#  1-Less or few lines of code
+#  2- Python is a multi-purpose programming language
+
+# Reasons 3 - 6
+# High Level Programming Language
+# Cross-Platform
+# Huge Community
+# Large Ecosystem
