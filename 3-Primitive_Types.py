c64987dbcbdf9c620a6839269c81e183d848a8c5
Vincent Oke <nisotus@gmail.com>
2022-09-09 09:02:32 +0200

    4th commit

---
+++ 3-Primitive_Types.py
@@ -0,0 +1,30 @@
+# PRIMITIVE TYPES IN PYTHON
+
+# *** VARIABLES
+
+# a = 100
+# b = 5000
+
+# sum = a + b
+
+# print(sum)
+
+# **************************************
+# *** STRINGS
+
+# course = "Python Programming"  # Double quotes
+
+# course = 'Software Testing'  # Single quotes
+# print(course)
+
+# Triple quotes
+message = """Python Programming
+
+is the best programming language
+
+to learn as it is fast growing
+
+and has a lot of applications
+
+"""
+print(message)
