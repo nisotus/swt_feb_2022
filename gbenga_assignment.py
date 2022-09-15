c2a0d3d31c0a197e2fcc32160b186ecd35bf7cef
Olugbenga Ajiga <47401804+authemany2k@users.noreply.github.com>
2022-09-16 00:28:21 +0100

    This is a program that print out specified name
    This is a program that prints out specified name from alphabet letters

---
+++ gbenga_assignment.py
@@ -0,0 +1,15 @@
+letter= { 1:'A', 2:'B', 3:'C', 4:'D', 5:'E', 6:'F', 7:'G', 8:'H', 9:'I', 
+            10:'J', 11:'K', 12:'L', 13:'M', 14:'N', 15:'O', 16:'P', 17:'Q', 
+            18:'R', 19:'S', 20:'T', 21:'U', 22:'V', 23:'W', 24:'X', 25:'Y', 26:'Z'}
+
+my_name_is = f"{letter[7]}{letter[2]}{letter[5]}{letter[14]}{letter[7]}{letter[1]}"
+
+print(my_name_is)
+
+my_brother_name_is = f"{letter[20]}{letter[15]}{letter[25]}{letter[5]}"
+
+print(my_brother_name_is)
+
+my_wife_name_is = f"{letter[20]}{letter[15]}{letter[13]}{letter[9]}"
+
+print(my_wife_name_is)
