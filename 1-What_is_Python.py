a6761e4d58dd0b51f933406cbcec6beb82c2303f
Michael Oni <oniolumuyiwa1@gmail.com>
2022-09-15 10:26:27 -0700

    Answers to assignment 1
    Assignment 1

--- 1-What_is_Python.py
+++ 1-What_is_Python.py
@@ -20,8 +20,8 @@
 #  print(fruit[1:-1])
 
 #hat is the result of this expression?
-m = 10 % 3
-print(m)
+# m = 10 % 3
+# print(m)
 # print(bool("False"))
 
 #  print(10 + 3)  # addition
@@ -34,3 +34,50 @@ print(m)
 
 #  # What will be printed to the terminal?
 #  print(bool("False"))
+
+
+
+'Floats', 'Integers', 'Boolean', 'Strings'
+
+
+fruit = "Apple"
+print(fruit[1])
+
+
+'p'
+
+
+fruit = "Apple"
+print(fruit[1:-1])
+
+
+'ppl'
+
+
+10 % 3
+
+
+'The expression returns 1'
+
+'hence the expression is false' 
+'because it leaves a remainder. 10 % 3 != 0'
+
+print(10 + 3) # addition
+13
+print(10 - 3) # subtraction
+7
+print(10 * 3) # multiplication
+30
+print(10 / 3) # division with remainder
+3.3333333333333335
+
+print(10 // 3) # division without remainder = Integer
+3
+print(10 % 3) # division to get only the remainder
+1
+print(10 ** 3) # exponent - 10 to the power of 3
+1000
+
+
+print(bool("False"))
+True
