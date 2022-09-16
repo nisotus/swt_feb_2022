c4f1402310027c3a8774567ec34ccd827c8279b7
duhney1 <dunniadesoba@gmail.com>
2022-09-16 10:19:57 +0200

    Solution to assignment 3

--- Assignment3.py
+++ Assignment3.py
@@ -6,3 +6,29 @@
 # I need to access the position of the alphabets that spells the name of the person I want spell
 # Then I need to save that name somewhere
 # Then I need to print it out
+
+
+Alphabet_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+print(Alphabet_list)
+first_letter = print(Alphabet_list[14])
+
+second_letter = print(Alphabet_list[11])
+
+third_letter = print(Alphabet_list[20])
+
+fourth_letter = print(Alphabet_list[12])
+
+fifth_letter = print(Alphabet_list[8])
+
+sixth_letter = print(Alphabet_list[3])
+
+seventh_letter = print(Alphabet_list[4])
+
+fullname= first_letter + second_letter + third_letter + fourth_letter + fifth_letter + sixth_letter + seventh_letter
+
+
+print(fullname)
+
+fullname = f"{first_letter} {second_letter} {third_letter} {fourth_letter} {fifth_letter} {sixth_letter} {seventh_letter}"
+
+print(fullname)
