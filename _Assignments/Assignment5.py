fc36cfa8d4f4b3cf24be6d05755f14e21f80c072
Vincent Oke <nisotus@yahoo.com>
2022-09-22 21:53:09 +0200

    Session-6

---
+++ _Assignments/Assignment5.py
@@ -0,0 +1,3 @@
+# SIMPLE CALCULATOR
+# Write a Python program that can perform addition, subtraction, multiplication and division based on two inputs from the user
+# So the program should perform all above arithmetic operations on any set two numbers you pass to it.
