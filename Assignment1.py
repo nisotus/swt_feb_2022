cc38c64f3ef974449a5c063a31deccdeeb1fd566
Michael Oni <oniolumuyiwa1@gmail.com>
2022-09-23 08:45:12 -0700

    Answer to Assignment 3

--- Assignment1.py
+++ Assignment1.py
@@ -4,20 +4,31 @@
 fruit = "Apple"
 print(fruit[1])
 
+'p'
+
 # What will be printed to the terminal?
 fruit = "Apple"
 print(fruit[1:-1])
+
+'Appl'
 
 # What is the result of this expression?
 10 % 3
 
 print(10 + 3)  # addition
+13
 print(10 - 3)  # subtraction
+7
 print(10 * 3)  # multiplication
+30
 print(10 / 3)  # division with remainder
+3.333
 print(10 // 3)  # division without remainder = Integer
+3
 print(10 % 3)  # division to get only the remainder
+1
 print(10 ** 3)  # exponent - 10 to the power of 3
-
+1000
 # What will be printed to the terminal?
 print(bool("False"))
+True
