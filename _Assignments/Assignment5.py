ab5efbe0835da9d6ac4671ad2f772da2793c4949
duhney1 <dunniadesoba@gmail.com>
2022-09-25 11:52:37 +0200

    Solution to assignments

--- _Assignments/Assignment5.py
+++ _Assignments/Assignment5.py
@@ -3,3 +3,21 @@
 # So the program should perform all above arithmetic operations on any set two numbers you pass to it.
 
 
+#Solution
+
+
+calculator1 = int(input("Enter the first number: "))
+calculator2 = int(input("Enter the second number: "))
+
+sum = calculator1 + calculator2
+
+subtraction = calculator1 - calculator2
+
+multiplication = calculator1 * calculator2
+division = calculator1 / calculator2
+
+print (sum)
+print(subtraction)
+print(multiplication)
+print(division)
+
--- _Assignments/Assignment6.py
+++ _Assignments/Assignment6.py
@@ -1,2 +1,3 @@
 # Write a Python program that will take the string below as input and split/separate the alphabets from the numbers
 # "My name is Will Brown and I am 48 years old, but my daughter is 17"
+
