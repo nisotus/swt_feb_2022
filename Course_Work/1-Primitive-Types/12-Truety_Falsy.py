de85d12314c4c3ac75aa13b8f0e96bc901e2be24
Vincent Oke <nisotus@yahoo.com>
2022-09-27 20:49:29 +0200

    Session-7

---
+++ Course_Work/1-Primitive-Types/12-Truety_Falsy.py
@@ -0,0 +1,14 @@
+# Truety values in Python - These are values that return "True"
+
+bool(5)
+bool("False")
+bool("True")
+bool(" ")
+
+
+# Falsy values in Python - There are values or expressions that return "False"
+# Empty string ("") is a Falsy and interpreted as a boolean false
+# The digit 0 or Number 0 is a Falsy and interpreted as boolean False
+# None: None represents the absence of a value. It is a Falsy and interpreted as a boolean False
+
+# But whenever we use the values ("", 0, None) in a boolean context, we get a False. Anything else will be True
---
+++ _Solutions/simple_calculator_2.py
@@ -0,0 +1,31 @@
+# SIMPLE CALCULATOR
+# Write a Python program that can perform addition, subtraction, multiplication and division based on two inputs from the user
+# The program should ask the user which of the four arithmetic operations below the user wants to perform:
+# addition
+# subtraction
+# multiplication
+# division
+
+# Based on user selection, the selected arithmetic operator is performed and the result should be printed to the terminal.
+
+
+print("What operation do you want? ")
+operator = input("Enter either +, -, * or /: ")
+
+n1 = float(input("Enter first number: "))
+n2 = float(input("Enter second number: "))
+
+if operator == "+":
+    print(n1, operator, n2, "=", n1 + n2)
+
+elif operator == "-":
+    print(n1, operator, n2, "=", n1 - n2)
+
+elif operator == "*":
+    print(n1, operator, n2, "=", n1 * n2)
+
+elif operator == "/":
+    print(n1, operator, n2, "=", n1 / n2)
+
+else:
+    print("Invalid Operator")
