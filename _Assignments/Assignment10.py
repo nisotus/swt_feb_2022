46dbe7269666379aa785e9630a3993c6772d4f2e
basee35@gmail.com <45149060+basee35@users.noreply.github.com>
2022-09-27 23:33:31 +0200

    Session 7 assignments

--- _Assignments/Assignment10.py
+++ _Assignments/Assignment10.py
@@ -7,3 +7,17 @@
 # division
 
 # Based on the selection of the operator by teh user, the selected arithmetic operator is performed and the result should be printed to the terminal.
+
+num1 = int(input("Enter first number:   "))
+num2 = int(input("Enter second number:  "))
+opr = input("""What do you want to do?
+    -   for subtraction   
+    +   for addition  
+    /   for division  
+    *   for multiplication    
+    """)
+
+(print(f'Subtraction: {num1} - {num2} = {num1 - num2}') if opr == '-' else 
+print(f'Addition: {num1} + {num2} = {num1 + num2}') if opr == '+' else
+print(f'Division: {num1} / {num2} = {num1 / num2}') if opr == '/' else
+print(f'Multiplication: {num1} * {num2} = {num1 * num2}'))
--- _Assignments/Assignment9.py
+++ _Assignments/Assignment9.py
@@ -7,3 +7,22 @@
 # division
 
 # Based on the selection of the operator by teh user, the selected arithmetic operator is performed and the result should be printed to the terminal.
+while(1):
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number:  "))
+    operation = input("""What do you want to do?
+    -   for subtraction   
+    +   for addition  
+    /   for division  
+    *   for multiplication    
+    """)
+
+    if (operation == '-'):
+        print(f'Subtraction: {num1} - {num2} = {num1 - num2}')
+    elif(operation == '+'):
+        print(f'Addition: {num1} + {num2} = {num1 + num2}')
+    elif(operation == '/'):
+        print(f'Division: {num1} / {num2} = {num1 / num2}')
+    elif(operation == '*'):
+        print(f'Multiplication: {num1} * {num2} = {num1 * num2}')
+    
