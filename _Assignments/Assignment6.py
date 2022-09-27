2025f3f271cbc62ebd5ef2295ad810df52904df3
basee35@gmail.com <45149060+basee35@users.noreply.github.com>
2022-09-28 00:21:42 +0200

    Assignment 6

--- _Assignments/Assignment6.py
+++ _Assignments/Assignment6.py
@@ -1,2 +1,11 @@
 # Write a Python program that will take the string below as input and split/separate the alphabets from the numbers
-# "My name is Will Brown and I am 48 years old, but my daughter is 17"
+text = "My name is Will Brown and I am 48 years old, but my daughter is 17"
+outputText = ""
+numbers = []
+for word in text.split(' '):
+    if word.isdigit():
+        numbers.append(int(word))
+    else:
+        outputText += (word + ' ')
+print (outputText)
+print (numbers)
