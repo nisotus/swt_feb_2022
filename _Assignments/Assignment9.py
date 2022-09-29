148af93e664de498b73717f5c3d7112d599e0e2d
Michael Oni <oniolumuyiwa1@gmail.com>
2022-09-28 19:30:49 -0700

    Update Assignment

---
+++ _Assignments/Assignment9.py
@@ -0,0 +1,9 @@
+# SIMPLE CALCULATOR USING IF, ELIF and ELSE
+# Write a Python program that can perform addition, subtraction, multiplication and division based on two inputs from the user
+# The program should ask the user which of the four arithmetic operations below the user wants to perform:
+# addition
+# subtraction
+# multiplication
+# division
+
+# Based on the selection of the operator by teh user, the selected arithmetic operator is performed and the result should be printed to the terminal.
