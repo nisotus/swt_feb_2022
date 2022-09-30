ab587d9fad068bc727b651bd1c9577c8595e2484
basee35@gmail.com <45149060+basee35@users.noreply.github.com>
2022-09-30 12:34:39 +0200

    staged

--- _Assignments/Assignment10.py
+++ _Assignments/Assignment10.py
@@ -8,16 +8,24 @@
 
 # Based on the selection of the operator by teh user, the selected arithmetic operator is performed and the result should be printed to the terminal.
 
-num1 = int(input("Enter first number:   "))
-num2 = int(input("Enter second number:  "))
-opr = input("""What do you want to do?
-    -   for subtraction   
-    +   for addition  
-    /   for division  
-    *   for multiplication    
-    """)
+# num1 = int(input("Enter first number:   "))
+# num2 = int(input("Enter second number:  "))
+# opr = input("""What do you want to do?
+#     -   for subtraction
+#     +   for addition
+#     /   for division
+#     *   for multiplication
+#     """)
 
-(print(f'Subtraction: {num1} - {num2} = {num1 - num2}') if opr == '-' else 
-print(f'Addition: {num1} + {num2} = {num1 + num2}') if opr == '+' else
-print(f'Division: {num1} / {num2} = {num1 / num2}') if opr == '/' else
-print(f'Multiplication: {num1} * {num2} = {num1 * num2}'))
+# (print(f'Subtraction: {num1 - num2}') if opr == '-' else
+# print(f'Addition: {num1 + num2}') if opr == '+' else
+# print(f'Division: {num1 / num2}') if opr == '/' else
+# print(f'Multiplication: {num1 * num2}'))
+
+
+student = True
+goodCredit = True
+highIncome = True
+
+print("Eligible") if ((not student) and (
+    goodCredit or highIncome)) else print("Not eligible")
