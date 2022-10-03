6fbe422e6b6ca7b1a061d3b17cf430abda5826ca
Jessica Enoo Aizotoigbe <jessyjoe2k@yahoo.com>
2022-10-03 12:59:54 +0200

    Jessica_1

--- Assignment1.py
+++ Assignment1.py
@@ -3,22 +3,23 @@
 
 # What will be printed to the terminal?
 fruit = "Apple"
-print(fruit[1])                                             # A
+print(fruit[1])                                             
 
 # What will be printed to the terminal?
 fruit = "Apple"
-print(fruit[1:-1])                                          # Appl
+print(fruit[1:-1])                                          
 
 # What is the result of this expression?
-10 % 3                                                      # 1
+10 % 3                                                      
 
-print(10 + 3)  # addition                                       13
-print(10 - 3)  # subtraction                                     7
-print(10 * 3)  # multiplication                                 30
-print(10 / 3)  # division with remainder                        3.333...
-print(10 // 3)  # division without remainder = Integer          3
-print(10 % 3)  # division to get only the remainder             1
-print(10 ** 3)  # exponent - 10 to the power of 3               1000
+print(10 + 3)  # addition                                       
+print(10 - 3)  # subtraction                                     
+print(10 * 3)  # multiplication                                 
+print(10 / 3)  # division with remainder                        
+print(10 // 3)  # division without remainder = Integer         
+print(10 % 3)  # division to get only the remainder             
+print(10 ** 3)  # exponent - 10 to the power of 3            
 
 # What will be printed to the terminal?
-print(bool("False"))                                          #  True
+print(bool("False"))                                          
+
--- Assignment3.py
+++ Assignment3.py
@@ -6,3 +6,18 @@
 # I need to access the position of the alphabets that spells the name of the person I want spell
 # Then I need to save that name somewhere
 # Then I need to print it out
+
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+print(alphabets)
+
+#printing Joshua
+print(alphabets[9].upper() +alphabets[14] +alphabets[18] +alphabets[7] +alphabets[20] +alphabets[0])
+
+#printing Favour
+print(alphabets[5].upper() +alphabets[0] +alphabets[21] +alphabets[14] +alphabets[20] +alphabets[17])
+
+#printing Isabella
+print(alphabets[8].upper() +alphabets[18] +alphabets[0] +alphabets[1] +alphabets[4] +alphabets[11]+alphabets[11]+alphabets[0] )
+
+#printing Michelle
+print(alphabets[12].upper() +alphabets[8] +alphabets[2] +alphabets[7] +alphabets[4] +alphabets[11]+alphabets[11]+alphabets[4])
