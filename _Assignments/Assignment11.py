71deced2b576dc4e9e081ce22eb36a4190ffab4d
Jessica Enoo Aizotoigbe <jessyjoe2k@yahoo.com>
2022-10-05 23:53:48 +0200

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
    On branch jessy
    Your branch is behind 'origin/jessy' by 38 commits, and can be fast-forwarded.
       (use "git pull" to update your local branch)
    
     Changes to be committed:
    	modified:   _Assignments/Assignment11.py

---
+++ _Assignments/Assignment11.py
@@ -0,0 +1,13 @@
+score = float(input("Enter Student Score: "))
+(print("Student Grade is F")if score >= 0 and score < 40  else 
+    print("Student Grade is D")if score >= 40 and score < 50 else 
+    print ("Student Grade is C") if score >= 50 and score < 65 else
+    print("Student Grade is B")if score >= 65 and score < 75 else
+    print("Student Grade is A")if score >= 75 and score <= 100
+            
+else
+     print("Invalid Score"))
+
+  
+   
+    
