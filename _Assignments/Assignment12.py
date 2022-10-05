89cb0995566f720fbced03943bccf7b3c5ad8c43
duhney1 <dunniadesoba@gmail.com>
2022-10-05 11:14:18 +0200

    solution to assignments

--- _Assignments/Assignment12.py
+++ _Assignments/Assignment12.py
@@ -1,3 +1,18 @@
 # Write a Python program that will take as input a value in SEK
 # Convert the SEK to Naira equivalent
 # Print the result in a very intuitive manner to the terminal
+
+sek = str(input("Enter the currency you want to convert: \n"))
+naira = str(input("Enter the new currency you wnt to convert to: \n"))
+
+exchange_rate = float(input("Enter the exchange rate of sek to naira: \n"))
+
+amount = float(input("Enter the amount that you want to convert: \n"))
+
+naira = exchange_rate * amount
+print("the naira value is ", naira  , "naira")
+
+
+
+
+
--- _Assignments/Assignment13.py
+++ _Assignments/Assignment13.py
@@ -1,3 +1,8 @@
 # Write a Python program that will take as input a value in celcius
-# Convert the value to Fahrenheit
+# Convert the value to Farenheit
 # Print the result in a very intuitive manner to the terminal
+
+
+celcius_value = float(input("Enter the celcius value: \n"))
+conversion = celcius_value * 1.8 + 32
+print("The fahrenheit value is ", conversion)
