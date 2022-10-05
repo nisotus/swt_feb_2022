c3f0825cfa538ad45388720e27a8526af6b69f3a
duhney1 <dunniadesoba@gmail.com>
2022-10-05 11:14:18 +0200

    solution to assignments

--- _Assignments/Assignment10.py
+++ _Assignments/Assignment10.py
@@ -7,3 +7,33 @@
 # division
 
 # Based on the selection of the operator by teh user, the selected arithmetic operator is performed and the result should be printed to the terminal.
+
+
+
+num1= int(input("Enter the first number: \n"))
+num2 =int( input("Enter the second number: \n"))
+
+operat = input("""Which of the four arithmetric operations do you want to perform?
+                 - for subtraction
+                 + for addition
+                 * for multiplication
+                 """)
+
+
+    
+subtraction = num1 - num2
+print(subtraction)
+addition = num1 + num2 
+print(addition)
+multiplication = num1 * num2
+print(multiplication)
+
+
+if operat == "-":
+    print(subtraction = {num1} - {num2})
+    
+elif operat == "+":
+    print(addition = {num1} + {num2})
+    
+else:
+    print(multiplication = {num1} * {num2})
