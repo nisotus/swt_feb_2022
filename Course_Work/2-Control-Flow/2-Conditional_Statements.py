c1f65541298751d95488d9ba172528f27f851349
Vincent Oke <nisotus@gmail.com>
2022-10-06 20:34:03 +0200

    fixing_3

--- Course_Work/2-Control-Flow/2-Conditional_Statements.py
+++ Course_Work/2-Control-Flow/2-Conditional_Statements.py
@@ -2,10 +2,10 @@
 
 # Below is an example
 
-# temperature = 35
-# if temperature > 30:
-#     print("It is warm")
-#     print("Drink some water")
+temperature = 35
+if temperature > 30:
+     print("It is warm")
+     print("Drink some water")
 
 # # This will be executed where our condition "if temperature > 30" is True or False
 # print("Done")
@@ -24,16 +24,16 @@
 
 # You can also have "else" statement to be execute if none of the "ifs" and "elifs" are True
 
-# temperature = 15
-# if temperature > 30:
-#     print("It is warm")
-#     print("Drink some water")
-# elif temperature > 20:
-#     print("It's nice weather")
-# else:
-#     print("its Cold")
+temperature = 15
+if temperature > 30:
+    print("It is warm")
+    print("Drink some water")
+elif temperature > 20:
+    print("It's nice weather")
+else:
+    print("its Cold")
 
-# print("Done")
+print("Done")
 
 
 # Grades from 0 to 100
@@ -60,16 +60,78 @@
 
 # Using "elifs"
 
-score = float(input("Enter Student Score: "))
+<<<<<<< HEAD
+#score = float(input("Enter Student Score: "))
 
-if score < 40:
-    print("Student Grade is F")
-elif score >= 40 and score < 50:
-    print("Student Grade is D")
-elif score >= 50 and score < 65:
-    print("Student Grade is C")
-elif score >= 65 and score < 75:
-    print("Student Grade is B")
-elif score >= 75 and score <= 100:
-    print("Student Grade is A")
+#if score < 40:
+ #   print("Student Grade is F")
+#elif score >= 40 and score < 50:
+   # print("Student Grade is D")
+#elif score >= 50 and score < 65:
+ #   print("Student Grade is C")
+#elif score >= 65 and score < 75:
+ #   print("Student Grade is B")
+#elif score >= 75 and score <= 100:
+ #   print("Student Grade is A")
+=======
+# try:
+#     score = float(input("Enter Student Score: "))
+#     if score >= 0 and score < 40:
+#         print("Student Grade is F")
+#     elif score >= 40 and score < 50:
+#         print("Student Grade is D")
+#     elif score >= 50 and score < 65:
+#         print("Student Grade is C")
+#     elif score >= 65 and score < 75:
+#         print("Student Grade is B")
+#     elif score >= 75 and score <= 100:
+#         print("Student Grade is A")
+#     else:
+#         print("Invalid Score")
+# except ValueError:
+#     print("Invalid Score")
 
+#************************************************************
+
+# score = float(input("Enter Student Score: "))
+# if score >= 0 and score < 40:
+#     print("Student Grade is F")
+# elif score >= 40 and score < 50:
+#     print("Student Grade is D")
+# elif score >= 50 and score < 65:
+#     print("Student Grade is C")
+# elif score >= 65 and score < 75:
+#     print("Student Grade is B")
+# elif score >= 75 and score <= 100:
+#     print("Student Grade is A")
+# else:
+#     print("Invalid Score")
+
+
+# if - will secure a category or partition of input
+
+# elif - each elif will also secure a different partition or category of input
+# You can have several elifs
+
+# else - will secure category of input that the if and elif cannot or did not secure
+
+
+score = input("Enter Student Score: ")
+
+if((score.isdigit())):
+
+    score = int(score)
+    if score >= 0 and score < 40:
+        print("Student Grade is F")
+    elif score >= 40 and score < 50:
+        print("Student Grade is D")
+    elif score >= 50 and score < 65:
+        print("Student Grade is C")
+    elif score >= 65 and score < 75:
+        print("Student Grade is B")
+    elif int(score) == score and score >= 75 and score <= 100:
+        print("Student Grade is A")
+  
+else:
+     print("Invalid Score")
+>>>>>>> 898f8345bab33cb63081945f1a2206d21e849893
