7e8b2e9b5374f86dc36117789accd06522d6a777
Vincent Oke <nisotus@gmail.com>
2022-10-06 22:17:05 +0200

    Session-10

---
+++ _Assignments/Assignment15.py
@@ -0,0 +1,12 @@
+# Write a Python program that can convert between any two currencies that you input from the terminal
+# ****************************************
+# Request for the customer's fullname and print it to the terminal
+# Request for the customer's ID number and create a conditional to verify if the ID number is valid
+# If the ID number is valid, do the following:
+# Ask which currency the customer will like to convert from? Take the response as input from the terminal
+# Ask which currency the customer will like to convert to? Take the response as input from the terminal
+
+# Based on the two currencies entered by the customer, perform currency conversion using the current live exchange rate
+# Print the conversion amount to the terminal
+
+# To get a live currency rate, you might need to use an API or any library that can help you to get live exchange rate online.
