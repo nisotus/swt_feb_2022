692942008f21a5bcfac12a3b8829b73ab87d687a
Vincent Oke <nisotus@gmail.com>
2022-10-20 21:06:11 +0200

    Session13 again

---
+++ _Assignments/Assignment19.py
@@ -0,0 +1,5 @@
+# Write a program that calculates the SUM of all the items in a shopping cart
+# Let's say we have a LIST of prices
+# prices = [10, 20, 30]
+
+# Use a function to write the same code
