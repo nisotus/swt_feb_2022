52494680f7eed1869bc574cfb671880f6b22183e
Vincent Oke <vincent.oke@ingka.ikea.com>
2022-10-26 07:23:59 +0200

    Session-14

---
+++ _Solutions/Solution_20.py
@@ -0,0 +1,21 @@
+# Write a program that implements below requirements
+# Use a function to write the code
+
+# if name is less than 3 characters long
+# display a validation error: "Name must be at least 3 characters"
+# otherwise if it's more than 50 characters long
+# display a validation error: "Name can be a maximum of 50 characters"
+# otherwise if it's between 3 and 50 characters
+# display: "Name looks good!"
+
+name = input("Enter a name: ")
+
+if len(name) < 3:
+    print("Error! Name must be at least 3 characters")
+elif len(name) > 50:
+    print("Error! Name can be a maximum of 50 characters")
+elif (len(name) >= 3) and (len(name) <=50):
+    print("Name looks good!")
+else:
+    pass
+    
