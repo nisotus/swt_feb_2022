6f268ebafced7f339b96ad11536ff4e4d50d68ca
nisotus <47868412+nisotus@users.noreply.github.com>
2022-11-03 20:06:13 +0100

    Session_18

---
+++ _Assignments/Assignment25.py
@@ -0,0 +1,3 @@
+# Read about Functional vs Object oriented programming
+# You can start with this blog: 
+# https://medium.com/@shaistha24/functional-programming-vs-object-oriented-programming-oop-which-is-better-82172e53a526
