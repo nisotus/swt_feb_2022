3d99ee0a1e190031e79f4dbbe8b111419dbf2b96
duhney1 <dunniadesoba@gmail.com>
2022-11-17 09:51:44 +0100

    update

--- _Assignments/Assignment35.py
+++ _Assignments/Assignment35.py
@@ -1,10 +1,10 @@
 # Use DICTIONARY COMPREHENSION to rewrite below code:
 
 vowels = {"a": 1, "e": 5, "i": 9, "o": 16, "u": 21}
-for v, w in vowels.items():
-    print(v, w * 2)
+# for v, w in vowels.items():
+#     print(v, w * 2)
     
 #code
 
-vowels = {v: w*2 for v in vowels}
-print(v)
+vowels = {v: w*2 for v, w in vowels.items()}
+print(vowels)
