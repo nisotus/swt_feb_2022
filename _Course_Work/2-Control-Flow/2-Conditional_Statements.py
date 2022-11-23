1f91844ef042da8e33ba039d4c256ca7f845f62a
duhney1 <dunniadesoba@gmail.com>
2022-11-23 10:53:55 +0100

    Assignment 37 solution

--- _Course_Work/2-Control-Flow/2-Conditional_Statements.py
+++ _Course_Work/2-Control-Flow/2-Conditional_Statements.py
@@ -24,38 +24,38 @@
 
 # You can also have "else" statement to be execute if none of the "ifs" and "elifs" are True
 
-temperature = 15
-if temperature > 30:
-    print("It is warm")
-    print("Drink some water")
-elif temperature > 20:
-    print("It's nice weather")
-else:
-    print("its Cold")
+# temperature = 15
+# if temperature > 30:
+#     print("It is warm")
+#     print("Drink some water")
+# elif temperature > 20:
+#     print("It's nice weather")
+# else:
+#     print("its Cold")
 
-print("Done")
+# print("Done")
 
 
-# Grades from 0 to 100
+#Grades from 0 to 100
 # A - 75 to 100
 # B - 65 to less than 75
 # C - 50 to 64
 # D - 40 to 49
 # F - Below 40
 
-# score = int(input("Enter Student Score: "))
-# score = float(input("Enter Student Score: "))
+score = int(input("Enter Student Score: "))
+score = float(input("Enter Student Score: "))
 
-# if score < 40:
-#     print("Student Grade is F")
-# if score >= 40 and score < 50:
-#     print("Student Grade is D")
-# if score >= 50 and score < 65:
-#     print("Student Grade is C")
-# if score >= 65 and score < 75:
-#     print("Student Grade is B")
-# if score >= 75 and score <= 100:
-#     print("Student Grade is A")
+if score < 40:
+    print("Student Grade is F")
+if score >= 40 and score < 50:
+    print("Student Grade is D")
+if score >= 50 and score < 65:
+    print("Student Grade is C")
+if score >= 65 and score < 75:
+    print("Student Grade is B")
+if score >= 75 and score <= 100:
+    print("Student Grade is A")
 
 
 # Using "elifs"
