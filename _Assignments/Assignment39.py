939bf8694102ed8df19ad9cd22eb27861d32d471
nisotus <47868412+nisotus@users.noreply.github.com>
2022-12-10 10:03:35 +0100

    Assignment-39

---
+++ _Assignments/Assignment39.py
@@ -0,0 +1,13 @@
+# Write a Python program using classes to create attributes and behaviours for different Animals
+# Create a base class called "Animal"
+# Make sure to show in your code that your "Animal" class inherits attributes and behaviour from the "Object" class
+# The "Animal" class should have the attributes: Eyes, Head, Ears, Nose, Legs,
+
+# The "Animal" class should have behaviours: Walk, Run, Eat, Jump, Swim, Cry, Sleep, Slither, Crawl, Bark, Roar, Hiss
+
+# Create additinal classes of animals including: Lion, Shark, Goat, Frog, Snake, Lizard, Dog
+
+# Think about how ou can craete the subclasses by category
+# For example, we know that fishes can swim and Dogs can bark, so craete the sub classes to capture all attributes ad behaviours above
+
+# Make sure you don't leave any attribute or behaviour out of your implimentation.
