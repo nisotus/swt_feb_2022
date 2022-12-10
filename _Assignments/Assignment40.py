11374e402fb58dde03ca683c6c5dc029e83c7008
nisotus <47868412+nisotus@users.noreply.github.com>
2022-12-10 12:42:11 +0100

    Assignment40

---
+++ _Assignments/Assignment40.py
@@ -0,0 +1,22 @@
+# Using the class "AgileSoftwareDevelopment" as a base class
+# Write a Python program that implements "ScrumSoftwareDevelopment" class as a sub-class to "AgileSoftwareDevelopment" base class
+# The "AgileSoftwareDevelopment" base class should implement the method "DeliverSoftware()"
+# The "ScrumSoftwareDevelopment" class should inherit the "AgileSoftwareDevelopment" base class and extend the class as below:
+
+# While there are items in the ProductBacklog, then we do below actions:
+
+# Sprint Planning Meeting
+# Sprint Backlog 
+# Daily Scrum Meeting
+# Development and Test
+# Sprint Review Meeting
+# Sprint Retrospective
+
+# Also implement the sprint to be 4 weeks or less
+# If more than 4 weeks, raise an exception or error message
+# Otherwise the sprint should continue for another 4 weeks till the end of the project
+# The total project length should be 6 months.
+
+
+
+
