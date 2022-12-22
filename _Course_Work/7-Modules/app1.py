550503f4f03c8c14b522a281ffa9e1c4d06dd4c4
nisotus <47868412+nisotus@users.noreply.github.com>
2022-12-22 20:04:34 +0100

    Session-37

--- _Course_Work/7-Modules/app1.py
+++ _Course_Work/7-Modules/app1.py
@@ -1,7 +1,1 @@
-import sales
-
-# When we run the "app.py" program, you will see a new folder called "__pycache__"
-# In the "__pycache__" folder we have a compiled version of the modules that we import into our program
-
-# *** Question
-# How does Python know if the compiled version is up-to-date with the latest code in the compiled module?
+from eCommerce.shopping import sales4
---
+++ _Course_Work/7-Modules/eCommerce/shopping/sales4.py
@@ -0,0 +1,9 @@
+def calc_tax():
+    pass
+
+
+def calc_shipping():
+    pass
+
+print("Sales4 Initialized")
+print(__name__)
